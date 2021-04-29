2c89b25f462a33237de9d3d8a62dd5c00b2cb35c
Breno Vergilio <bjvergilio47@gmail.com>
2021-04-28 21:08:48 -0400

    Cadastro de usuarios implementado

--- test/system/signups_test.rb
+++ test/system/signups_test.rb
@@ -2,13 +2,13 @@ require "application_system_test_case"
 
 class SignupsTest < ApplicationSystemTestCase
   test "usuario faz cadastro com dados validos" do 
-    visti "/"
+    visit "/"
     click_on "Cadastre-se"
 
     fill_in "Nome", with: "Darth Vader"
     fill_in "E-mail", with: "vader@imperio.com"
     fill_in "Senha", with: "minhasenha"
-    fill_in "Confirme sua senha", with: "minhasenha"
+    fill_in "Confirmação da senha", with: "minhasenha"
 
     click_on "Cadastrar"
 
